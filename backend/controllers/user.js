42a58a82624bebeb5f0028d04733efe1fcddebb5
Luis Marte <luisdavidmarte00@gmail.com>
2024-02-05 22:33:26 -0400

    creacion y actualizacion de usuarios

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,33 +1,41 @@
 const userModel = require('../models/user')
 
 var userController = {
-    home: (req,res)=>{
+    home: async (req,res)=>{
         return res.status(200).send({response:params})
     },
     createUser: async (req, res) => {
         const { nombre, email, password } = req.body;
-    
-        try {
+
+        try{
+            // Se hace una solicitud asincrona para determinar si el usuario ya existe
+            const usuarioExistente = await userModel.findOne({where:{email:email}})
+            //Si este existe no se creara uno nuevo
+            if (usuarioExistente){
+               return res.status(404).send({error:"Este correo ya esta siendo utilizado"})
+            }
+
             const user = await userModel.create({
                 nombre: nombre,
                 email: email,
                 password: password
-            });
-    
-            return res.status(200).send({ user: user });
-        } catch (error) {
-            console.error('Error al crear el usuario:', error);
-            return res.status(500).send({ message: 'Ha ocurrido un problema con el servidor' });
-        }
-    },
-    updateUser:(req,res)=>{
-        const { id, email, password } = req.body;
-        try{
-            userModel.update()
+            })
+
+            return res.status(200).send({user:user})
         }
         catch(err){
-            console.log(`Ha ocurrido un error ${err}`)
+            res.status(500).send({message:`Ha ocurrido un error con la soliciutd ${err}`})  
         }
+       
+    },
+    updateUser:(req,res)=>{
+        const id = req.params.id;
+        const userNewData = req.body;
+        userModel.update(userNewData, { where:{id_user: id} })
+        .then((updatedUser)=>{
+            return res.status(200).send({updatedUser:updatedUser})
+        })
+    
     }
 }
 
